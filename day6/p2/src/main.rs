8393fa00131d54a7d4b0eb10498c051928eaf8b2
guidofe <guido.ferri@protonmail.com>
2022-12-06 17:40:17 +0100

    Day 6

---
+++ day6/p2/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(iter_advance_by)]
+use std::{fs, collections::HashSet};
+//use itertools::Itertools;
+
+// Only iterators, because why not
+fn main() {
+    let chars: Vec<char> = fs::read_to_string("../input").expect("Couldn't read file").chars().collect();
+    let (n, _) = chars.windows(14).enumerate().find(|(_, slice)| {
+        let set: HashSet<char> = slice.iter().copied().collect();
+        return set.len() == 14;
+    }).unwrap();
+    println!("{}", n + 14);
+
+
+}
